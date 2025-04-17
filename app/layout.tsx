f3d225e2cfd4fbb1a6ea0c1b93ef44e5a97716ac
Aditya Akbar <adityakbare.connect@gmail.com>
2025-04-18 02:49:08 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,31 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Invoise - Automating Billing",
   description: "Create and manage invoices easily with multi-currency support",
-   icons:{
+  icons: {
     icon: "/favicon.png", 
-  }, 
+  },
+  openGraph: {
+    title: "Invoise - Automating Billing",
+    description: "Create and manage invoices easily with multi-currency support",
+    url: "https://invoise.vercel.app",
+    siteName: "Invoise",
+    images: [
+      {
+        url: "https://invoise.vercel.app/Invoise.png",
+        width: 1200,
+        height: 630,
+        alt: "Invoise preview image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Invoise - Automating Billing",
+    description: "Create and manage invoices easily with multi-currency support",
+    images: ["https://invoise.vercel.app/Invoise.png"],
+  },
 }
 
 export default function RootLayout({
@@ -28,7 +50,4 @@ export default function RootLayout({
       </body>
     </html>
   )
-}
-
-
-import './globals.css'
+} 
